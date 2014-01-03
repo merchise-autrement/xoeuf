99043ce9de7f444683bc2e01769bce8a850fc4a3
Medardo Rodriguez <med@merchise.org>
2014-01-03 13:06:00 -0500

    Defines `store_identity` function.
    To be used in ``store`` parameter for functional fields when monitor is
    needed for fields in a local model.

--- xoeuf/osv/orm.py
+++ xoeuf/osv/orm.py
@@ -2,7 +2,7 @@
 #----------------------------------------------------------------------
 # xoeuf.osv.orm
 #----------------------------------------------------------------------
-# Copyright (c) 2013 Merchise Autrement and Contributors
+# Copyright (c) 2013, 2014 Merchise Autrement and Contributors
 # All rights reserved.
 #
 # This is free software; you can redistribute it and/or modify it under
@@ -17,6 +17,20 @@ from __future__ import (division as _py3_division,
                         print_function as _py3_print,
                         unicode_literals as _py3_unicode,
                         absolute_import)
+
+
+def store_identity(self, cr, uid, ids, context=None):
+    '''To be used in ``store`` parameter for functional fields when monitor
+    is needed for fields in a local model.
+
+    For example::
+
+        'active':
+            fields.function(_get_active, type='boolean', string='Active?',
+                store={_name: (store_identity, ['contract_ids'], 10), }),
+
+    '''
+    return ids
 
 
 def search_read(self, cr, uid, *args, **kwargs):
