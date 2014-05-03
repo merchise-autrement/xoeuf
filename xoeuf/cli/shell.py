55376695d7c22854d97587bd3f6c9a00fce85494
Manuel Vázquez Acosta <mva.led@gmail.com>
2014-05-02 23:03:54 -0400

    A simple `xoeuf shell` command.

---
+++ xoeuf/cli/shell.py
@@ -0,0 +1,27 @@
+# -*- encoding: utf-8 -*-
+#----------------------------------------------------------------------
+# xoeuf.cli.shell
+#----------------------------------------------------------------------
+# Copyright (c) 2014 Merchise Autrement and Contributors
+# All rights reserved.
+#
+# This is free software; you can redistribute it and/or modify it under the
+# terms of the LICENCE attached (see LICENCE file) in the distribution
+# package.
+#
+# Created on 2014-05-02
+
+
+from __future__ import (division as _py3_division,
+                        print_function as _py3_print,
+                        unicode_literals as _py3_unicode,
+                        absolute_import as _py3_abs_import)
+
+
+from xoutil.cli import Command
+
+
+class Shell(Command):
+    def run(self, argv=None):
+        from IPython import start_ipython
+        start_ipython(argv=argv)
