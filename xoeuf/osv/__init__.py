b89fe62cd3bc8b0965d7cb762078462f11f27c02
Manuel Vázquez Acosta <mva.led@gmail.com>
2014-11-12 19:21:30 -0500

    Make savepoint aware of Odoo v8.

--- xoeuf/osv/__init__.py
+++ xoeuf/osv/__init__.py
@@ -23,25 +23,33 @@ from __future__ import (division as _py3_division,
                         print_function as _py3_print,
                         absolute_import as _py3_abs_import)
 
-import contextlib
 
-
-@contextlib.contextmanager
 def savepoint(cr, name=None):
     '''A context manager that enters a new savepoint.
 
     If `name` is not provided a random one is generated.
 
+    .. note:: In Odoo v8, the cursor `cr` has already a ``savepoint`` method.
+              This function will fallback to it in this case.  However, the
+              argument `name` will be ignored in this case.
+
     '''
-    if not name:
-        from xoutil.uuid import uuid
-        name = uuid(True)
-    cr.execute('SAVEPOINT "%s"' % name)
-    try:
-        yield
-    except:
-        cr.execute('ROLLBACK TO SAVEPOINT "%s"' % name)
-        raise
-    else:
-        cr.execute('RELEASE SAVEPOINT "%s"' % name)
-del contextlib
+    from xoutil import Unset
+    _savepoint = getattr(cr, 'savepoint', Unset)
+    if _savepoint is Unset:
+        from contextlib import contextmanager
+        if not name:
+            from xoutil.uuid import uuid
+            name = uuid(True)
+
+        @contextmanager
+        def _savepoint():
+            cr.execute('SAVEPOINT "%s"' % name)
+            try:
+                yield
+            except:
+                cr.execute('ROLLBACK TO SAVEPOINT "%s"' % name)
+                raise
+            else:
+                cr.execute('RELEASE SAVEPOINT "%s"' % name)
+    return _savepoint()
