7087645edf054045c1172f4b3a28f5a0bff1fecf
Manuel Vázquez Acosta <mva.led@gmail.com>
2015-01-05 11:35:11 -0500

    Allow to use the api.v8, api.v7 idiom.

--- xoeuf/api.py
+++ xoeuf/api.py
@@ -45,3 +45,41 @@ def contextual(func):
         with Environment.manage():
             return func(*args, **kwargs)
     return inner
+
+
+try:
+    from openerp.api import v8, v7  # noqa
+except ImportError:
+    from xoutil.decorator import aliases
+
+    @aliases('v8', 'v7')
+    def vX(f):
+        '''Mimic the `api.v8` and `api.v7` behaviour in Odoo.
+
+        Odoo has introduced a new API for model methods.  Basically it has
+        removed the ``cr, uid, ..., context=None`` boilerplate.  See the
+        ``api.py`` file in the branch 8.0 of Odoo for details.
+
+        However, to ease the upgrade path, they allow to keep using both
+        signatures.  Furthermore a model may provide two different
+        implementations for both versions by using the `api.v8` and `api.v7`
+        decorators like in::
+
+           @api.v8
+           def browse(self, ids):
+               pass
+
+           @api.v7
+           def browse(self, cr, uid, ids, context=None):
+               pass
+
+        This simple method allows to *mimic* this idiom without the semantics
+        it implies.
+
+        .. warning:: The order of use is very important.
+
+           Since we simply return the function as is.  When this is run in
+           OpenERP you must place the v7 after the v8.
+
+        '''
+        return f
