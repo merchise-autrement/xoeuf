e9ffed9a22b62eb62066da1c99d321d2b92ad5bc
Manuel Vázquez Acosta <mva.led@gmail.com>
2015-03-03 10:11:28 -0500

    Control the encoding only when needed.

--- xoeuf/cli/mailgate.py
+++ xoeuf/cli/mailgate.py
@@ -18,13 +18,14 @@ XMLRPC.
 
 from __future__ import (division as _py3_division,
                         print_function as _py3_print,
-                        unicode_literals as _py3_unicode,
                         absolute_import as _py3_abs_import)
 
 
 from . import Command
 from logging import Handler
 
+
+from xoutil.string import safe_encode, safe_decode
 
 # TODO: This has grown into a monstrous pile of code that needs
 # refactorization.
@@ -33,10 +34,9 @@ from logging import Handler
 # TODO: Should this be moved elsewhere?
 class SysLogHandler(Handler):
     def emit(self, report):
-        # This avoids the /dev/log system issue and goes directly to Unix.
-        # But then Windows is f.cked.
+        # This avoids the /dev/log system issue and goes directly to Unix
+        # syslog.  But then Windows is f.cked.
         import syslog
-        from xoutil.string import safe_encode
         syslog.syslog(safe_encode(self.format(report)))
 
 del Handler
@@ -52,11 +52,13 @@ class Mailgate(Command):
 
     '''
 
-    MESSAGE_TEMPLATE = ("Error while processing message with id {msgid} "
-                        "from {sender}.\n\n"
-                        "{traceback}\n\n"
-                        "{details_title}:\n\n"
-                        "{message_details}\n")
+    MESSAGE_TEMPLATE = safe_encode(
+        "Error while processing message with id {msgid} "
+        "from {sender}.\n\n"
+        "{traceback}\n\n"
+        "{details_title}:\n\n"
+        "{message_details}\n"
+    )
 
     @classmethod
     def get_arg_parser(cls):
@@ -211,7 +213,6 @@ class Mailgate(Command):
         import logging
         import email
         from email.message import Message
-        from xoutil.string import safe_encode, safe_decode
         logger = logging.getLogger(__name__)
         tb = traceback.format_exc()
         if not isinstance(message, Message):
@@ -219,23 +220,24 @@ class Mailgate(Command):
         else:
             msg = message
             message = msg.as_string()
-        msgid = safe_decode(msg.get('Message-Id', '<NO ID>'))
-        sender = safe_decode(msg.get('Sender', msg.get('From', '<nobody>')))
-        # Make the message a string, so that the logger does not fail but the
-        # encoding might by wrong, since the original byte-stream encoding is
-        # not known.  Avoid doing anything fancy like parsing the Message with
-        # all its Content-Type and Content-Type-Encoding complexities.  Those
-        # complexities are left to the message processing OpenERP has to do.
-        details = safe_decode(message)
+        msgid = safe_encode(msg.get('Message-Id', '<NO ID>'))
+        sender = safe_encode(msg.get('Sender', msg.get('From', '<nobody>')))
+        # Make the message a string (bytes), so that the logger does not fail
+        # but the encoding might by wrong, since the original byte-stream
+        # encoding is not known.  Avoid doing anything fancy like parsing the
+        # Message with all its Content-Type and Content-Type-Encoding
+        # complexities.  Those complexities are left to the message processing
+        # OpenERP has to do.
+        details = safe_encode(message)
         details_title = 'Raw message'
         report = cls.MESSAGE_TEMPLATE.format(
             msgid=msgid,
             sender=sender,
             traceback=tb,
             details_title=details_title,
-            message_details=details
+            message_details=details,
         )
-        logger.error(safe_encode(report, 'ascii'))
+        logger.error(report)
 
     def run(self, args=None):
         from openerp import SUPERUSER_ID
@@ -275,7 +277,13 @@ class Mailgate(Command):
             else:
                 print(str('5.0.0 Permanent error. System error.'),
                       file=sys.stderr)
-            self.send_error_notification(message or 'No message provided')
+            # First decode the message since the raw message may contain
+            # invalid UTF-8 sequences or come with a mixture of encodings
+            # (each MIME part can be encoded differently) and thus the logger
+            # fail to construct the log message.
+            self.send_error_notification(
+                safe_decode(message) if message else 'No message provided'
+            )
             sys.exit(1)
 
     def read_conffile(self, filename):
