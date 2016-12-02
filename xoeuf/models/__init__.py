5116737a1bd08835db5f1dd609f6e2f9f0228c61
Manuel Vázquez Acosta <manu@merchise.org>
2016-12-02 12:23:31 -0500

    Re-export get_modelname from 'xoeuf.models'.

--- xoeuf/models/__init__.py
+++ xoeuf/models/__init__.py
@@ -22,3 +22,4 @@ from __future__ import (division as _py3_division,
                         absolute_import as _py3_abs_import)
 
 from . import proxy  # noqa: ensure the hook is registered.
+from xoeuf.osv.orm import get_modelname  # noqa: re-export
