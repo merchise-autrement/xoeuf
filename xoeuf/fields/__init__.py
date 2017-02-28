c8bdd9b2116f1b29e55137bdffe7bd7370b10a27
Manuel Vázquez Acosta <manu@merchise.org>
2017-02-28 11:12:38 -0500

    Fix flake.

--- xoeuf/fields/__init__.py
+++ xoeuf/fields/__init__.py
@@ -17,4 +17,4 @@ from __future__ import (division as _py3_division,
                         absolute_import as _py3_abs_import)
 
 
-from .datetime import LocalizedDatetime
+from .datetime import LocalizedDatetime  # noqa: reexport
