merchise-autrement/xoeuf

commit bee7c0a35ddc7864e24af0004dedface89bd37a8

Actually commit the reset passwords.
Since commit d72f077 the command 'xoeuf secure' does not commit the passwords
after changing them.

xoeuf/cli/secure.py
@@ -106,7 +106,8 @@ class Secure(Command):
         if conffile:
             self.read_conffile(conffile)
         self = options.database
-        reset_all_passwords(self, security_level=level)
+        with self.evn.cr:
+            reset_all_passwords(self, security_level=level)
 
     def read_conffile(self, filename):
         import os

commit 0983312039b5392a3289145b8f2ee3edba2b1455

Fix typo introduced in bee7c0a35ddc7

xoeuf/cli/secure.py
@@ -106,7 +106,7 @@ class Secure(Command):
         if conffile:
             self.read_conffile(conffile)
         self = options.database
-        with self.evn.cr:
+        with self.env.cr:
             reset_all_passwords(self, security_level=level)
 
     def read_conffile(self, filename):