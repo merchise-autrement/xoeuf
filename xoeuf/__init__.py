c4a48414548dc26cc39c3314440abc07e68bb359
Manuel Vázquez Acosta <manu@merchise.org>
2017-06-28 14:35:46 -0400

    Remove usage of unicode_literals.

--- xoeuf/__init__.py
+++ xoeuf/__init__.py
@@ -22,7 +22,6 @@ The name is composed by:
 
 from __future__ import (division as _py3_division,
                         print_function as _py3_print,
-                        unicode_literals as _py3_unicode,
                         absolute_import as _py3_abs_import)
 
 
