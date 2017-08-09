merchise-autrement/xoeuf

commit eedff031a2ab10253e3278b969ef1e7c8ad11b3b

Ensure new LocalizedDatetime gets its surrogate fields.
Solves issue MERCURIO-125 [1].

This seems to have been taken care for Odoo 10, but not for Odoo 8.

.. [1] http://sentry.lahavane.com/caraibes/mercurio/issues/3670/

xoeuf/fields/datetime/_dt10.py
@@ -17,17 +17,13 @@ from __future__ import (division as _py3_division,
                         absolute_import as _py3_abs_import)
 
 
-try:
-    from openerp.release import version_info as ODOO_VERSION_INFO
-    from openerp import fields
-except ImportError:
-    from odoo.release import version_info as ODOO_VERSION_INFO
-    from odoo import fields
+from xoeuf.odoo.release import version_info as ODOO_VERSION_INFO
 assert ODOO_VERSION_INFO >= (9, 0)
 del ODOO_VERSION_INFO
 
 import pytz
 
+from xoeuf.odoo import fields
 from xoeuf.tools import localtime_as_remotetime
 
 
@@ -62,6 +58,8 @@ class LocalizedDatetime(fields.Datetime):
 
     def __init__(self, dt_field=None, tzone_field=None, **kwargs):
         # Include store=False if is not include in kwargs
+        if not dt_field or tzone_field:
+            raise TypeError('LocalizedDatetime requires the surrogates fields')
         self.dt_field = dt_field
         self.tzone_field = tzone_field
         kwargs = dict(
@@ -72,7 +70,10 @@ class LocalizedDatetime(fields.Datetime):
         super(LocalizedDatetime, self).__init__(**kwargs)
 
     def new(self, **kwargs):
-        # pass origin field args to the new one.
+        # Pass original args to the new one.  This ensures that the
+        # tzone_field and dt_field are present.  In odoo/models.py, Odoo calls
+        # this `new()` without arguments to duplicate the fields from parent
+        # classes.
         kwargs = dict(self.args, **kwargs)
         return super(LocalizedDatetime, self).new(**kwargs)
 

xoeuf/fields/datetime/_dt8.py
@@ -16,7 +16,7 @@ from __future__ import (division as _py3_division,
                         print_function as _py3_print,
                         absolute_import as _py3_abs_import)
 
-from openerp.release import version_info as ODOO_VERSION_INFO
+from xoeuf.odoo.release import version_info as ODOO_VERSION_INFO
 assert (8, 0) <= ODOO_VERSION_INFO < (9, 0)
 del ODOO_VERSION_INFO
 
@@ -110,6 +110,8 @@ class LocalizedDatetime(fields.Datetime):
         self.depends = (self.dt_field, self.tzone_field)
 
     def __init__(self, dt_field=None, tzone_field=None, **kwargs):
+        if not dt_field or tzone_field:
+            raise TypeError('LocalizedDatetime requires the surrogates fields')
         self.dt_field = dt_field
         self.tzone_field = tzone_field
         # Include store=False and copy=False if not already included.
@@ -117,3 +119,11 @@ class LocalizedDatetime(fields.Datetime):
         super(LocalizedDatetime, self).__init__(
             dt_field=dt_field, tzone_field=tzone_field, **kwargs
         )
+
+    def new(self, **kwargs):
+        # Ensures that the tzone_field and dt_field are present.  In
+        # openerp/models.py, Odoo calls this `new()` without arguments to
+        # duplicate the fields from parent classes.
+        kwargs = dict(dict(tzone_field=self.tzone_field,
+                           dt_field=self.dt_field), **kwargs)
+        return super(LocalizedDatetime, self).new(**kwargs)

commit c8e6dda81d98189a50128bbea3b2d12374e7b15c

Correct LocalizedDatetime expectations.
Fixes bug introduced in eedff031a2ab10253e3278b969ef1e7c8ad11b3b.  We require
both attributes.

xoeuf/fields/datetime/_dt10.py
@@ -58,7 +58,7 @@ class LocalizedDatetime(fields.Datetime):
 
     def __init__(self, dt_field=None, tzone_field=None, **kwargs):
         # Include store=False if is not include in kwargs
-        if not dt_field or tzone_field:
+        if not dt_field or not tzone_field:
             raise TypeError('LocalizedDatetime requires the surrogates fields')
         self.dt_field = dt_field
         self.tzone_field = tzone_field

xoeuf/fields/datetime/_dt8.py
@@ -110,7 +110,7 @@ class LocalizedDatetime(fields.Datetime):
         self.depends = (self.dt_field, self.tzone_field)
 
     def __init__(self, dt_field=None, tzone_field=None, **kwargs):
-        if not dt_field or tzone_field:
+        if not dt_field or not tzone_field:
             raise TypeError('LocalizedDatetime requires the surrogates fields')
         self.dt_field = dt_field
         self.tzone_field = tzone_field
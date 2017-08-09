c8e6dda81d98189a50128bbea3b2d12374e7b15c
Manuel Vázquez Acosta <manu@merchise.org>
2017-08-09 12:18:20 -0400

    Correct LocalizedDatetime expectations.
    Fixes bug introduced in eedff031a2ab10253e3278b969ef1e7c8ad11b3b.  We require
    both attributes.

--- xoeuf/fields/datetime/_dt8.py
+++ xoeuf/fields/datetime/_dt8.py
@@ -110,7 +110,7 @@ class LocalizedDatetime(fields.Datetime):
         self.depends = (self.dt_field, self.tzone_field)
 
     def __init__(self, dt_field=None, tzone_field=None, **kwargs):
-        if not dt_field or tzone_field:
+        if not dt_field or not tzone_field:
             raise TypeError('LocalizedDatetime requires the surrogates fields')
         self.dt_field = dt_field
         self.tzone_field = tzone_field
