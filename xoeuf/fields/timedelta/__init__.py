ea026287715456ea3c338f9384dd4f1f1d3c5c5d
Johanny <johanny@merchise.org>
2017-09-29 14:56:27 -0400

    Add TimeDelta field.

---
+++ xoeuf/fields/timedelta/__init__.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# --------
+# __init__
+# --------
+# Copyright (c) 2017 Merchise Autrement [~º/~] and Contributors
+# All rights reserved.
+#
+# This is free software; you can redistribute it and/or modify it under the
+# terms of the LICENCE attached (see LICENCE file) in the distribution
+# package.
+#
+# Created on 2017-09-29
+
+
+from __future__ import (division as _py3_division,
+                        print_function as _py3_print,
+                        absolute_import as _py3_abs_import)
+
+from xoeuf import MAJOR_ODOO_VERSION
+
+if MAJOR_ODOO_VERSION in (8, 9, 10,):
+    from .timedelta import TimeDelta  # noqa: reexport
+else:
+    raise NotImplementedError
+
+TimeDelta.__module__ = __name__
