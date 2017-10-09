60427273ab213118c36ab6d8ddfdfbf4f4bc9ec4
Johanny <johanny@merchise.org>
2017-10-09 13:22:11 -0400

    Add domain tests.

---
+++ xoeuf/tests/test_domain/__manifiest__.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# ---------------------------------------------------------------------
+# __openerp__
+# ---------------------------------------------------------------------
+# Copyright (c) 2017 Merchise Autrement [~º/~] and Contributors
+# All rights reserved.
+#
+# This is free software; you can redistribute it and/or modify it under the
+# terms of the LICENCE attached (see LICENCE file) in the distribution
+# package.
+#
+# Created on 2017-10-04
+
+{
+    'name': 'test_domain',
+    'author': 'Merchise Autrement [~º/~] and Contributors',
+    'description': 'Test for the domain type',
+    'depends': ['base', ],
+    'installable': True,
+    'auto_install': False,
+}
---
+++ xoeuf/tests/test_domain/tests/test_domain.py
@@ -0,0 +1,155 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# ---------------------------------------------------------------------
+# test_timespan
+# ---------------------------------------------------------------------
+# Copyright (c) 2017 Merchise Autrement [~º/~] and Contributors
+# All rights reserved.
+#
+# This is free software; you can redistribute it and/or modify it under the
+# terms of the LICENCE attached (see LICENCE file) in the distribution
+# package.
+#
+
+from __future__ import (division as _py3_division,
+                        print_function as _py3_print,
+                        absolute_import as _py3_abs_import)
+
+from odoo.tests.common import TransactionCase
+from xoeuf.osv.expression import Domain
+
+
+class TestDomain(TransactionCase):
+
+    def test_eq(self):
+        A = Domain([
+            ('field_x', '=', 1)
+        ])
+        # A == A
+        self.assertTrue(A == A)
+
+        # -A == -A
+        self.assertTrue(-A == -A)
+
+        # (x = 1)  == ! (x <> 1)
+        A1 = Domain([
+            '!',
+            ('field_x', '<>', 1)
+        ])
+        self.assertTrue(A == A1)
+
+    def test_imply(self):
+
+        # Basic implications.
+        # -------------------
+
+        A = Domain([
+            ('field_x', '=', 1)
+        ])
+        B = Domain([
+            ('field_y', '!=', False)
+        ])
+
+        # A => A
+        self.assertTrue(A.imply(A))
+
+        # A => A | B
+        self.assertTrue(A.imply(A | B))
+
+        # A & B => A
+        self.assertTrue((A & B).imply(A))
+
+        # A & B => A | B
+        self.assertTrue((A & B).imply(A | B))
+
+        # Inter terms implications.
+        # -------------------------
+
+        # (x = 1)  => (x = 1)
+        A = Domain([
+            ('field_x', '=', 1)
+        ])
+        A1 = Domain([
+            ('field_x', '=', 1)
+        ])
+        self.assertTrue(A.imply(A1))
+
+        # (x != 1)  => (x <> 1)
+        A = Domain([
+            ('field_x', '!=', 1)
+        ])
+        A1 = Domain([
+            ('field_x', '<>', 1)
+        ])
+        self.assertTrue(A.imply(A1))
+
+        # (x > 1) => (x > 0)
+        A = Domain([
+            ('field_x', '>', 10)
+        ])
+        A1 = Domain([
+            ('field_x', '>', 1)
+        ])
+        self.assertTrue(A.imply(A1))
+
+        # (x in (1,2,3)) => (x in (1,2,3,4))
+        A = Domain([
+            ('field_x', 'in', (1, 2, 3))
+        ])
+        A1 = Domain([
+            ('field_x', 'in', (1, 2, 3, 4))
+        ])
+        self.assertTrue(A.imply(A1))
+        #
+
+        # Compound implications.
+        # ----------------------
+        # A => B & C iff (A => B) & (A => C)
+        A = Domain([
+            ('field_x', 'in', (1, 4))
+        ])
+        B = Domain([
+            ('field_x', 'in', (1, 2, 4))
+        ])
+        C = Domain([
+            ('field_x', 'in', (1, 3, 4))
+        ])
+        self.assertTrue(A.imply(B))
+        self.assertTrue(A.imply(C))
+        self.assertTrue(A.imply(B & C))
+
+        # More complex implications
+        # -------------------------
+
+        x = Domain([
+            '|',
+            ('field_y', '!=', False),
+            ('field_x', '=', 'value'),
+            ('field_z', 'in', (1,))
+        ])
+        y = Domain([
+            '|',
+            ('field_y', '!=', False),
+            ('field_x', '=', 'value'),
+            ('field_z', 'in', (1, 2, 3)),
+            '|',
+            ('field_x', '=', 'value'),
+            ('field_y', '!=', False)
+        ])
+        assert x.imply(y)
+
+        x = Domain([
+            ('field_y', '!=', False),
+            ('field_z', 'in', (1,)),
+            ('field_z', 'in', (1, 2)),
+            '|',
+            ('field_y', '!=', False),
+            '|',
+            ('field_z', 'in', (1,)),
+            ('field_z', 'in', (2,))
+        ])
+        y = Domain([
+            ('field_y', '!=', False),
+            ('field_z', 'in', (1, 2, 3))
+        ])
+        assert x.imply(y)
