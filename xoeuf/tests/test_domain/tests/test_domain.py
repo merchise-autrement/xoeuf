f7dc8102e9a66d027acfde12e4110833a10a6d31
Manuel Vázquez Acosta <manu@merchise.org>
2017-10-11 11:38:16 -0400

    Test that 1NF and 2NF are idempotent.

--- xoeuf/tests/test_domain/tests/test_domain.py
+++ xoeuf/tests/test_domain/tests/test_domain.py
@@ -12,11 +12,62 @@ from __future__ import (division as _py3_division,
                         absolute_import as _py3_abs_import)
 
 import unittest
+from hypothesis import strategies as s, given
+
 from xoeuf.osv import expression as expr
 from xoeuf.osv.expression import Domain
 
+names = s.text(alphabet='abdefgh', min_size=1, average_size=3)
+operators = s.sampled_from(['=', '!=', '<', '>', '<>'])
+
+# Logical connectors with the amount of terms it connects.  Notice that ''
+# takes two arguments because it's the same as '&'.
+connectors = s.sampled_from([('', 2), ('&', 2), ('!', 1), ('|', 2)])
+
+
+@s.composite
+def terms(draw, values=s.integers(min_value=-10, max_value=10)):
+    name = str(draw(names))
+    operator = draw(operators)
+    value = draw(values)
+    return (name, operator, value)
+
+
+@s.composite
+def domains(draw, min_size=1, average_size=4):
+    leaves = draw(s.lists(
+        terms(),
+        min_size=min_size,
+        average_size=average_size
+    ))
+    result = []
+    while leaves:
+        connector, many = draw(connectors)
+        if many <= len(leaves):
+            if connector:
+                result.append(connector)
+            result.extend(leaves[:many])
+            leaves = leaves[many:]
+        elif len(leaves) == 1:
+            result.append(leaves.pop(0))
+    return Domain(result)
+
 
 class TestDomain(unittest.TestCase):
+    @given(domains())
+    def test_first_normal_form_idempotency(self, domain):
+        self.assertEqual(
+            domain.first_normal_form,
+            domain.first_normal_form.first_normal_form
+        )
+
+    @given(domains())
+    def test_second_normal_form_idempotency(self, domain):
+        self.assertEqual(
+            domain.second_normal_form,
+            domain.second_normal_form.second_normal_form
+        )
+
     def test_eq(self):
         A = Domain([
             ('field_x', '=', 1)
