22d414bed6f605221d2f42dc7cc2042df0597e84
Manuel Vázquez Acosta <manu@merchise.org>
2017-10-13 13:01:46 -0400

    Use new module headers.

--- xoeuf/fields/timedelta/timedelta.py
+++ xoeuf/fields/timedelta/timedelta.py
@@ -1,18 +1,11 @@
 #!/usr/bin/env python
-# -*- encoding: utf-8 -*-
-# -------------------------------------------------------------
-# xhg.autrement.xhg_autrement_operations_program.models._fields
-# -------------------------------------------------------------
-# Copyright (c) 2015-2017 Merchise Autrement [~º/~] and Contributors
+# -*- coding: utf-8 -*-
+# ---------------------------------------------------------------------
+# Copyright (c) Merchise Autrement [~º/~] and Contributors
 # All rights reserved.
 #
-# This is free software; you can redistribute it and/or modify it under the
-# terms of the LICENCE attached (see LICENCE file) in the distribution
-# package.
+# This is free software; you can do what the LICENCE file allows you to.
 #
-# Created on 2015-12-17
-
-'''Implement custom ORM fields.'''
 
 from __future__ import (division as _py3_division,
                         print_function as _py3_print,
@@ -29,7 +22,6 @@ class TimeDelta(Float):
     Python code we get a timedelta object.
 
     '''
-
     type = 'timedelta'
 
     @staticmethod
