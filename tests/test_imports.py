a17b5a129fc110ea3dab4a4a375f83628b1f0531
Manuel Vázquez Acosta <manu@merchise.org>
2017-10-16 09:30:44 -0400

    Verify xoeuf is importable.

---
+++ tests/test_imports.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# ---------------------------------------------------------------------
+# Copyright (c) Merchise Autrement [~º/~] and Contributors
+# All rights reserved.
+#
+# This is free software; you can do what the LICENCE file allows you to.
+#
+
+from __future__ import (division as _py3_division,
+                        print_function as _py3_print,
+                        absolute_import as _py3_abs_import)
+
+
+def test_imports():
+    'Xoeuf is importable in Python 2 and 3'
+    from xoeuf import fields, models, api  # noqa
