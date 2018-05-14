5c7b477965bc32616a2540b02e24faf8b5eb8f4e
Manuel Vázquez Acosta <manuel@merchise.org>
2018-05-14 12:25:10 -0400

    Show the code in the docs.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -47,6 +47,7 @@ extensions = ['sphinx.ext.autodoc',
               'sphinx.ext.doctest',
               'sphinx.ext.intersphinx',
               'sphinx.ext.todo',
+              'sphinx.ext.viewcode',
               'sphinx.ext.coverage']
 
 # Add any paths that contain templates here, relative to this directory.
