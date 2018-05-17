30be1e785a3f8f25c4bcce931f742af1abefffd9
Manuel Vázquez Acosta <manuel@merchise.org>
2018-05-17 09:32:16 -0400

    Stop looking for odoo.signals.
    In the next releases of our distribution of Odoo, we won't be shipping the
    signals module.  It was included there to allow our modifications use it.  But
    it's not used anymore.

--- xoeuf/tests/test_signals/models.py
+++ xoeuf/tests/test_signals/models.py
@@ -6,17 +6,11 @@
 #
 # This is free software; you can do what the LICENCE file allows you to.
 #
-
 from __future__ import (division as _py3_division,
                         print_function as _py3_print,
                         absolute_import as _py3_abs_import)
 
-
-# BEWARE that we test xoeuf implementation of signals, that's the reason of
-# this import.
-import xoeuf._signals_impl as signals
-
-from xoeuf import models, fields
+from xoeuf import models, fields, signals
 
 
 class SignalingModel(models.Model):
