9dbcb79ccb73f1fa80e023839d334bb8f42f293a
Manuel Vázquez Acosta <manuel@merchise.org>
2019-03-01 09:07:47 -0500

    Unify unit and integration tests.
    The tox is too slow creating the environments (pulling from the local Odoo git
    repo).

---
+++ xoeuf/tests/test_unit/__manifest__.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# ---------------------------------------------------------------------
+# Copyright (c) Merchise Autrement [~º/~] and Contributors
+# All rights reserved.
+#
+# This is free software; you can do what the LICENCE file allows you to.
+#
+{
+    'name': 'test_unit',
+    'author': 'Merchise Autrement [~º/~] and Contributors',
+    'description': 'Unit tests',
+    'depends': ['base', ],
+    'installable': True,
+    'auto_install': False,
+}
---
+++ xoeuf/tests/test_unit/tests/__init__.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# ---------------------------------------------------------------------
+# Copyright (c) Merchise Autrement [~º/~] and Contributors
+# All rights reserved.
+#
+# This is free software; you can do what the LICENCE file allows you to.
+#
+# flake8: noqa
+from __future__ import division, print_function, absolute_import
+
+from . import test_imports
+from . import test_localtime_as_remotetime
+from . import test_timerange_object
