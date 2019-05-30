0bc0fa7485a71323ee98de759418b833adb5308e
Manuel Vázquez Acosta <manuel@merchise.org>
2019-05-30 19:01:14 -0400

    Show that Property's type is not registered in the FIELD_TYPES selection.

---
+++ xoeuf/tests/test_fields_integration_with_reports/__manifest__.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# ---------------------------------------------------------------------
+# Copyright (c) Merchise Autrement [~º/~] and Contributors
+# All rights reserved.
+#
+# This is free software; you can do what the LICENCE file allows you to.
+#
+
+{
+    "name": "test_fields_integration_with_reports",
+    "version": "1.0",
+    "depends": ["base"],
+    "auto_install": False,
+    "installable": True,
+}
